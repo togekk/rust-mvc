7e45ded29f28e8b41f5432044018d1840ead409d
togekk <togekk@gmail.com>
2018-04-03 18:36:02 +0800

    first commit

---
+++ build.bat
@@ -0,0 +1,6 @@
+del /S /Q dist && ^
+cargo +nightly web deploy --target=wasm32-unknown-unknown --release && ^
+wasm-gc target/deploy/rust.wasm -o opt/rust.gc.wasm && ^
+wasm-opt opt/rust.gc.wasm -o opt/rust.wasm && ^
+npx parcel build target/deploy/index.html --public-url ./ && ^
+copy opt\rust.wasm dist
