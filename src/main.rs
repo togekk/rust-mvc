59c3a9a8770476d89ae51ec8d7ad125365a805f2
togekk <togekk@gmail.com>
2018-04-15 20:08:46 +0800

    Fix one line textnode display issue

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use]
 extern crate stdweb;
 
 use std::collections::HashMap;
@@ -5,10 +6,18 @@ use std::collections::HashMap;
 mod mvc;
 
 fn main() {
+    let html =
+        "<div (class)=\"red\">Number:
+            <span>{{num}}</span>
+            <span>Name:</span>
+            <span>{{name}}</span>
+            <span>Book Name:</span>
+            <span>{{book}}</span>
+        </div>";
     let mut scope: HashMap<&str, &str> = HashMap::new();
     scope.insert("num", "22");
     scope.insert("name", "John");
     scope.insert("book", "Harry Potter");
     scope.insert("age", "30");
-    mvc::mvc(&scope);
+    mvc::mvc(html, &scope);
 }
--- src/mvc.rs
+++ src/mvc.rs
@@ -1,37 +1,57 @@
 extern crate stdweb;
 
 use std::collections::HashMap;
+use stdweb::unstable::TryInto;
 use stdweb::web::{
     document,
-    INode
+    HtmlElement,
+    INode,
+    Node,
+    NodeList,
+    DocumentFragment
 };
+use std::vec::Vec;
 
-pub fn mvc(scope: &HashMap<&str, &str>) {
+pub fn mvc(_html: &str, scope: &HashMap<&str, &str>) {
 
     let body = document().body().unwrap();
     let node_list = body.child_nodes();
-    render_models(node_list, scope);
+    let frag = document().create_document_fragment().unwrap();
+    // let aaa = node_list.item(1).unwrap().get_attribute("(class)").unwrap();
+    // let item = document().create_text_node(&aaa);
+    let mut scope_found_id: Vec<u32> = Vec::new();
+    render_models(node_list, scope, &frag, &mut scope_found_id);
+    if remove_nodes(&body, body.child_nodes(), scope_found_id) {
+        body.append_child(&frag);
+    };
 }
 
-fn render_models(node_list: stdweb::web::NodeList, scope: &HashMap<&str, &str>) {
+fn render_models(node_list: NodeList, scope: &HashMap<&str, &str>, frag: &DocumentFragment, mut scope_found_id: &mut Vec<u32>) -> bool {
+    
     let len = node_list.len();
+    let mut found = false;
+    let mut a = false;
 
     for i in 0..len {
         let node = node_list.item(i).unwrap();
 
-        let child_node_list = node.child_nodes();
-        if child_node_list.len() > 1 {
-            render_models(child_node_list, scope);
-        };
-        // let parent_node = node.parent_node().unwrap();
+                if get_type(node_list.item(i).unwrap()) == 8 {
+            let node_comment = node_list.item(i).unwrap();
+            js!(@{node_comment}.textContent = "");
+        }
+
         let text = node.text_content().unwrap();
 
         if let Some(_start) = text.find("{{") {
-            // let node_new = document().create_element(&node.node_name()).unwrap();
-            // while (node.first_child().is_null()) {
-                node.remove_child(&node.first_child().unwrap()).unwrap();
-            // }
+            found = true;
+            scope_found_id.push(i);
 
+            match get_type(node_list.item(i).unwrap()) {
+                1 => {
+                    node.remove_child(&node.first_child().unwrap()).unwrap();
+                },
+                _ => (),
+            }
 
             let split = text.split("{{");
 
@@ -43,27 +63,68 @@ fn render_models(node_list: stdweb::web::NodeList, scope: &HashMap<&str, &str>) 
                     match scope.get(t) {
                         Some(scope_found) => {
                             let item = document().create_text_node(&scope_found);
-                            node.append_child(&item);
+                            frag.append_child(&item);
                         },
                         None => println!("")
                     }
 
                     let rest = &s[end+2..];
-                    let item = document().create_text_node(&rest);
-                    node.append_child(&item);
+                    if get_type(node_list.item(i).unwrap()) != 8 {
+                        let item = document().create_text_node(&rest);
+                        frag.append_child(&item);
+                    }
 
                 } else {
-
                     let item = document().create_text_node(&s);
-                    node.append_child(&item);
-                    
+                    frag.append_child(&item);
                 }
 
             }
 
+        } else {
+            if !a {
+                let item = document().create_text_node(&text);
+                frag.append_child(&item);
+            }
         }
 
-        
+        let child_node_list = node.child_nodes();
+        if child_node_list.len() > 1 {
+            a = render_models(child_node_list, scope, frag, &mut scope_found_id);
+        };
+
+
 
     }
+
+    return found;
+}
+
+fn remove_nodes(body: &HtmlElement, node_list: NodeList, scope_found_id: Vec<u32>) -> bool {
+    let len = node_list.len();
+
+    let mut a: Vec<u32> = Vec::new();
+
+    for i in 0..len {
+        if scope_found_id.contains(&i) {
+            if get_type(node_list.item(i).unwrap()) != 3  {
+                a.push(i);
+            } else {
+                let node = node_list.item(i).unwrap();
+                js!(@{node}.textContent = "");
+            }
+        }
+    }
+
+    for i in 0..a.len() {
+        body.remove_child(&node_list.item(a[i]).unwrap()).unwrap();
+    }
+    
+    return true;
+}
+
+fn get_type(node: Node) -> u32 {
+    let a = js!( return @{node}.nodeType );
+    let b: u32 = a.try_into().unwrap();
+    return b;
 }
