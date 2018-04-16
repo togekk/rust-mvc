13aeddbc9898fd8b2fdaa76c1cc589cd57d04414
togekk <togekk@gmail.com>
2018-04-16 10:11:07 +0800

    Add branch for DocumentFragment demo

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
-#[macro_use]
 extern crate stdweb;
 
-use std::collections::HashMap;
-
-mod mvc;
+use stdweb::web::{
+    document,
+    INode,
+    INonElementParentNode
+};
 
 fn main() {
-    let mut scope: HashMap<&str, &str> = HashMap::new();
-    scope.insert("num", "22");
-    scope.insert("name", "John");
-    scope.insert("book", "Harry Potter");
-    scope.insert("age", "30");
-    mvc::mvc(&scope);
+    let frag = document().create_document_fragment().unwrap();
+
+    for _i in 0..10 {
+        let div = document().create_element("div").unwrap();
+        let text = document().create_text_node("test");
+        div.append_child(&text);
+        frag.append_child(&div);
+    }
+
+    let el = document().get_element_by_id("to_be_append").unwrap();
+    el.append_child(&frag);
 }
