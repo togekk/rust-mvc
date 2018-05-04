4696be45956b269cd8755145778bfd1a8c8151d9
togekk <togekk@gmail.com>
2018-05-05 00:15:57 +0800

    Use replace instead of removing & appending; add getting attribute functions

--- src/mvc.rs
+++ src/mvc.rs
@@ -1,7 +1,7 @@
 extern crate stdweb;
 
 use std::collections::HashMap;
-use std::vec::Vec;
+use stdweb::unstable::TryInto;
 use stdweb::web::{document, INode, IParentNode, Node, NodeList};
 
 pub fn render(html: &str, scope: &HashMap<&str, String>) {
@@ -11,41 +11,46 @@ pub fn render(html: &str, scope: &HashMap<&str, String>) {
     js!(@{&div}.innerHTML = @{&html});
 
     let node_list = div.child_nodes();
-    let mut mvc_node_list: Vec<Node> = Vec::new();
     let count: i64 = 0;
-    if render_models(node_list, &mut mvc_node_list, scope, count) {
+    if render_models(node_list, scope, count) {
         frag.append_child(&div);
         if app.has_child_nodes() {
-            app.remove_child(&app.first_child().unwrap()).unwrap();
+            app.replace_child(&frag, &app.first_child().unwrap())
+                .unwrap();
+        } else {
+            app.append_child(&frag);
         }
-        app.append_child(&frag);
     };
 }
 
-fn render_models(
-    node_list: NodeList,
-    mvc_node_list: &mut Vec<Node>,
-    scope: &HashMap<&str, String>,
-    mut count: i64,
-) -> bool {
+fn render_models(node_list: NodeList, scope: &HashMap<&str, String>, mut count: i64) -> bool {
     let len = node_list.len();
 
     for i in 0..len {
         let node = node_list.item(i).unwrap();
         let mut text = node.text_content().unwrap();
 
+        let attr_len = get_length(&node);
+        for i in 0..attr_len {
+            let a = get_item(&node, i);
+            match a {
+                Some(attr) => console!(log, get_value(&attr)),
+                None => {}
+            }
+        }
+
         let child_node_list = node.child_nodes();
         if child_node_list.len() > 0 {
             count += 1;
-            render_models(child_node_list, mvc_node_list, scope, count);
+            render_models(child_node_list, scope, count);
             count -= 1;
         } else {
+            // Models
             for (key, val) in scope {
                 let mut key_new: String = "{{".to_owned();
                 key_new.push_str(key);
                 key_new.push_str("}}");
                 text = text.replace(&key_new, val);
-                mvc_node_list.push(node.clone());
             }
 
             js!(@{&node}.textContent = @{text});
@@ -57,4 +62,28 @@ fn render_models(
 
 pub fn set_scope(scope: &mut HashMap<&str, String>, key: &str, val: &str) {
     *scope.get_mut(key).unwrap() = val.to_owned();
+}
+
+fn get_length(node: &Node) -> u32 {
+    js!(
+        var a = 0;
+        if (!!@{&node}.attributes)
+        a = @{&node}.attributes.length;
+        return a;
+    ).try_into()
+        .unwrap()
+}
+
+fn get_item(node: &Node, index: u32) -> Option<Node> {
+    js!(
+        var a;
+        if (!!@{&node}.attributes)
+        a = @{node}.attributes[@{index}];
+            return a;
+        ).try_into()
+        .unwrap()
+}
+
+fn get_value(attr: &Node) -> String {
+    js!(return @{attr}.value).try_into().unwrap()
 }
