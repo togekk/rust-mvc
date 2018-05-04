7c37b771c755c4d5c91def23687600bc23f29eae
togekk <togekk@gmail.com>
2018-05-04 15:53:53 +0800

    Update values instead of create new hashmap

--- src/main.rs
+++ src/main.rs
@@ -17,22 +17,20 @@ fn main() {
     scope.insert("x", "null".to_owned());
     scope.insert("y", "null".to_owned());
     mvc::render(HTML, &scope);
+    let mut scope_2 = scope.clone();
 
     window().add_event_listener(move |event: MouseMoveEvent| {
         let x: String = (f64::from(event.client_x()) * 3.141592654).to_string();
         let y: String = (f64::from(event.client_y()) * 3.141592654).to_string();
-        let mut scope = HashMap::new();
-        scope.insert("name", "John".to_owned());
-        scope.insert("x", x);
-        scope.insert("y", y);
+        *scope.get_mut("x").unwrap() = x;
+        *scope.get_mut("y").unwrap() = y;
         mvc::render(HTML, &scope);
     });
 
     window().add_event_listener(move |_: ClickEvent| {
-        let mut scope = HashMap::new();
-        scope.insert("name", "David".to_owned());
-        scope.insert("x", "click!".to_owned());
-        scope.insert("y", "click!".to_owned());
-        mvc::render(HTML, &scope);
+        *scope_2.get_mut("name").unwrap() = "David".to_owned();
+        *scope_2.get_mut("x").unwrap() = "click!".to_owned();
+        *scope_2.get_mut("y").unwrap() = "click!".to_owned();
+        mvc::render(HTML, &scope_2);
     });
 }
